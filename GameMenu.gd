ddf94ee458bfefcfb843ec5975371ec94b207578
Joseph Sharratt <joseph.sharratt@gmail.com>
2024-02-18 14:15:50 +0000

    OrcZomb uses NavigationAgent3D in NavigationRegion3D mesh to follow the player

--- GameMenu.gd
+++ GameMenu.gd
@@ -2,6 +2,10 @@ extends Control
 
 @onready var start_button = get_node("StartButton")
 
+func _ready():
+# can remove this for prod mode
+	self.visible = false
+
 func _on_start_button_pressed():
 	print("you pressed the start button")
 	self.visible = false
--- OrcZomb.gd
+++ OrcZomb.gd
@@ -2,9 +2,10 @@ extends CharacterBody3D
 
 var health_points : int = 3
 var max_health : int = 3
-var attackDistance : float = 1.0
-var awarenessRadius : float = 5.0
-var moveSpeed : float = 2.0
+var attack_distance : float = 1.0
+var awarenessRadius : float = 500.0
+var move_speed : float = 2.0
+var movement_target_position: Vector3
 var gravity : float = 15.0
 var knockback = Vector3.ZERO
 var attackRate : float = 1.0
@@ -15,23 +16,36 @@ var attackRate : float = 1.0
 @onready var weaponAnimation = get_node("Model/WeaponHolder/WeaponAnimator")
 @onready var showDamageAnimation = get_node("Model/ShowDamageAnimator")
 @onready var attackRayCast = get_node("Model/AttackRayCast")
+@onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
 
 func _ready () :
 	#set the timer wait time
 	$HealthBar3D.update_health_bar(health_points, max_health)
 	timer.wait_time = attackRate # see if this can be timer.set_wait_time(attackRate)
 	timer.start()
+	
+	# These values need to be adjusted for the actor's speed
+	# and the navigation layout.
+	navigation_agent.path_desired_distance = 0.5
+	navigation_agent.target_desired_distance = 0.5
+
+	# Make sure to not await during _ready.
+	call_deferred("actor_setup")
 
 func _physics_process(delta):
 	var distanceToPlayer = position.distance_to(player.position)
-	var shouldFollowPlayer = distanceToPlayer < awarenessRadius # && distanceToPlayer > attackDistance
+	var shouldFollowPlayer = distanceToPlayer < awarenessRadius # && distanceToPlayer > attack_distance
 
 	if shouldFollowPlayer:
-		var direction = (player.position - position).normalized()
+		var current_agent_position: Vector3 = global_position
+		var next_path_position: Vector3 = navigation_agent.get_next_path_position()
 		
-		velocity.x = direction.x * moveSpeed + knockback.y
-		velocity.z = direction.z * moveSpeed + knockback.x
+		set_movement_target(player.global_position)
+
+		velocity = current_agent_position.direction_to(next_path_position) * move_speed
 		
+		var direction = (next_path_position - position).normalized()
+
 		var facing_angle = Vector2(direction.z, direction.x).angle()
 		self.rotation.y = lerp_angle(self.rotation.y, facing_angle, 0.5)
 
@@ -41,6 +55,16 @@ func _physics_process(delta):
 
 	move_and_slide()
 	knockback = lerp(knockback, Vector3.ZERO, 0.1)
+
+func actor_setup():
+	# Wait for the first physics frame so the NavigationServer can sync.
+	await get_tree().physics_frame
+
+	# Now that the navigation map is no longer empty, set the movement target.
+	set_movement_target(player.global_position)
+
+func set_movement_target(movement_target: Vector3):
+	navigation_agent.set_target_position(movement_target)
 
 func receive_damage (damage):
 	health_points -= damage
@@ -53,7 +77,7 @@ func receive_shove (force, shove_direction):
 	knockback = shove_direction * force
 
 func _on_timer_timeout():
-	if position.distance_to(player.position) <= attackDistance:
+	if position.distance_to(player.position) <= attack_distance:
 		weaponAnimation.stop()
 		weaponAnimation.play("Slash")
 		player.receive_damage(1)
