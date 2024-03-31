e01888b561a490e4c11f1d6b65dceaa88bc4510b
Joseph Sharratt <joseph.sharratt@gmail.com>
2024-03-31 14:40:19 +0100

    1 second delay to show Game Over screen

--- GameManager.gd
+++ GameManager.gd
@@ -14,7 +14,7 @@ signal game_over
 @onready var hud = get_node("UI/HUD")
 
 var wave_count : int = 0
-var player_count : int = 2
+var player_count : int = 1
 
 var enemy_wave_sequence : Array = [
 	[{ "x": 18, "z": -6 }],
--- GameOverMenu.gd
+++ GameOverMenu.gd
@@ -9,6 +9,7 @@ func _process(delta):
 		get_tree().reload_current_scene()
 
 func _on_main_game_over():
+	await get_tree().create_timer(1).timeout
 	self.visible = true
 	await get_tree().create_timer(1).timeout
 	any_key_info.visible = true
